da5ba11804d7e977bed29130198412e81e30c71c
dregna <81594379+dregna@users.noreply.github.com>
2025-10-10 00:37:33 -0500

    feat: enable prerendering for SEO so Helmet meta tags appear in static HTML output

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "@/components/layout/Layout";
+import Home from "@/pages/Home";
+import Offers from "@/pages/offers";
+import RoboAdvisors from "@/pages/RoboAdvisors";
+import HowItWorks from "@/pages/HowItWorks";
+import BrokerDeepDivePage from "@/pages/broker/[slug]";
+
+export default function AppRoutes() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/robo-advisors" element={<RoboAdvisors />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/broker/:slug" element={<BrokerDeepDivePage />} />
+      </Routes>
+    </Layout>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
 import "./index.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+const app = (
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>
 );
+
+if (rootElement?.hasChildNodes()) {
+  hydrateRoot(rootElement, app);
+} else {
+  const root = createRoot(rootElement);
+  root.render(app);
+}
+
+if (typeof window !== "undefined" && typeof document !== "undefined") {
+  const announceReady = () => {
+    document.dispatchEvent(new Event("prerender-ready"));
+  };
+
+  if ("requestIdleCallback" in window) {
+    window.requestIdleCallback(announceReady);
+  } else {
+    window.setTimeout(announceReady, 0);
+  }
+}
